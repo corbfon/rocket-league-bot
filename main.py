ab6a211e6fe8f68b5b90401c995fd72d3c4cbddd
BuildTools <unconfigured@null.spigotmc.org>
2022-10-28 12:36:18 -0500

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+# This file is for strategy
+
+from util.objects import *
+from util.routines import *
+
+
+
+class Bot(GoslingAgent):
+    # This function runs every in-game tick (every time the game updates anything)
+    def run(self):
+        if self.get_intent() is not None:
+            return
+        def boost_sorter(boost):
+            return (self.me.location - boost.location).magnitude()
+        active_boosts = [boost for boost in self.boosts if boost.active]
+        active_boosts.sort(key=boost_sorter)
+        self.set_intent(goto_boost(active_boosts[0]))
